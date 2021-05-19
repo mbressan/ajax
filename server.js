e0de89b238ae3ed085f8030cef3997cf93c69560
Mateus Bressan <mateusnh@gmail.com>
2021-05-19 20:20:35 -0300

    Usando fetch e await em formulario

--- server.js
+++ server.js
@@ -33,5 +33,13 @@ app.post('/upload', (req, res) => {
     })
 })
 
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+
 app.get('/teste', (req, res) => res.send('OK!'))
 app.listen(8080, ()=> console.log('Executando...'))
